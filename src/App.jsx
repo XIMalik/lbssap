d06605acd49d520e57b5e705b1ef001b54339557
XIMalik <abdulmalikawesu@gmail.com>
2023-04-12 15:23:15 +0100

    init

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,26 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import Skills from "./pages/Skills/Skills";
+import Landing from "./pages/Landing/Landing";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Formtest from "./pages/FormTest/Formtest";
+import Course from "./pages/Course/Course";
+Course;
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div>
-      <h1 className="text-5xl ">LBSSAP SITE</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<Landing />} />
+        <Route exact path="/skills" element={<Skills />}>
+          <Route exact path="/skills/course" element={<Course />} />
+        </Route>
+        <Route exact path="/form" element={<Formtest />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
